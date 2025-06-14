60c3a71865adbcbf5a665d23d85e2f3493554122
chinhanchen <aaric1120@hotmail.com>
2025-06-14 09:24:03 -0400

    added diagonal reward

--- src/main/java/com/scratchgame/app/Reward.java
+++ src/main/java/com/scratchgame/app/Reward.java
@@ -8,11 +8,15 @@ import java.util.stream.Collectors;
 public class Reward {
     private GenMatrix matrix_obj;
     private int bet_amount;
+    private int row;
+    private int column;
     private HashMap<String, ArrayList<String>> winElements = new HashMap<>();
 
     public Reward(GenMatrix input_matrix, String betamount) {
         matrix_obj = input_matrix;
         bet_amount = Integer.parseInt(betamount);
+        row = matrix_obj.getDimensions()[0];
+        column = matrix_obj.getDimensions()[1];
     }
 
     public HashMap<String, ArrayList<String>> getWinElements() {
@@ -20,6 +24,7 @@ public class Reward {
     }
 
     public void checkWin(){
+        // check for symbol counts
         for(String key:matrix_obj.getCount().keySet()) {
             if (matrix_obj.getCount().get(key) > 2) {
                 winElements.put(key, new ArrayList<>());
@@ -27,14 +32,37 @@ public class Reward {
                         entry.getValue().getCount() == matrix_obj.getCount().get(key)).map(Map.Entry::getKey).collect(Collectors.toList()).get(0));
             }
         }
+
+        //check for straight lines
+        // check for diagoanal lines
+        checkDiagonal();
     }
 
     public void checkstraight() {
 
     }
 
-    public void checkHorizontal() {
+    public void checkDiagonal() {
+        // left to right
+        String curr_sym = matrix_obj.getMatrix().get(String.format("%d_%d", 0,0));
+        for (int i = 1; i <= column; i++) {
+            if (!curr_sym.equals(matrix_obj.getMatrix().get(String.format("%d_%d",i ,i)))) {
+                break;
+            } else if (i == column) {
+                winElements.get(curr_sym).add("same_symbols_diagonally_left_to_right");
+            }
 
+        }
+
+        // right to left
+        curr_sym = matrix_obj.getMatrix().get(String.format("%d_%d", 0,column));
+        for (int i = 1; i <= row;i++) {
+            if (!curr_sym.equals(matrix_obj.getMatrix().get(String.format("%d_%d",i ,column - i)))) {
+                break;
+            } else if (i == row) {
+                winElements.get(curr_sym).add("same_symbols_diagonally_right_to_left");
+            }
+        }
     }
 
     public Integer calculateReward(){
